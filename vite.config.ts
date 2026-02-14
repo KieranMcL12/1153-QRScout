97625fd2b8312b17c0b821ca2bb567991b52d4ed
Kieran McL <98069614+KieranMcL12@users.noreply.github.com>
2026-02-14 15:18:08 -0500

    try fix again

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/QRScout/',
+  base: '/1153-QRScout/',
   plugins: [react()],
   resolve: {
     alias: {
