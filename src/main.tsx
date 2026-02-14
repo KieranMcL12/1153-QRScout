8eba142268e01a2b60db724621a61bd2bc79170b
Kieran McL <98069614+KieranMcL12@users.noreply.github.com>
2026-02-14 14:57:18 -0500

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import { App } from 'src/app.tsx';
-import 'src/index.css';
+import { App } from './app.tsx';
+import './index.css';
 
 const root = createRoot(document.getElementById('app')!);
 
