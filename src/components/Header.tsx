6642c160b14a6221216340f9c38bd4a85bc200dd
Kieran McL <98069614+KieranMcL12@users.noreply.github.com>
2026-02-14 16:02:36 -0500

    logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <Helmet>
       <title>QRScout | {page_title}</title>
-      <link rel="icon" href="/1153-QRScout/favicon.ico" />
+      <link rel="icon" href="/QRScout/favicon.ico" />
     </Helmet>
   );
 }
