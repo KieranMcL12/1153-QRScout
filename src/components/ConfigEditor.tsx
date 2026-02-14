22a8a37608f6ed6e3c9b6cdd7b1234b76d01ad90
Kieran McL <98069614+KieranMcL12@users.noreply.github.com>
2026-02-14 16:09:36 -0500

    try to fix

--- src/components/ConfigEditor.tsx
+++ src/components/ConfigEditor.tsx
@@ -77,7 +77,7 @@ export function ConfigEditor(props: ConfigEditorProps) {
       validate: true,
       schemas: [
         {
-          uri: 'https://kieranmcl12.github.io/1153-QRScout/schema.json',
+          uri: 'https://frc2713.github.io/QRScout/schema.json',
           fileMatch: ['*'],
           schema,
         },
